6ede13fa6e91135e7bb9e13aaad61eac18c5fef1
abdu <abdualkerim01@gmail.com>
2026-02-20 10:13:33 +0300

    1st commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let person = {
+    name: "Abdu",
+    age: 24,
+    country: "saudi",
+};
+
+function logData() {
+    console.log(
+        person.name +
+        " is " +
+        person.age +
+        " years old " +
+        "and lives in " +
+        person.country
+    );
+}
+
+logData();
