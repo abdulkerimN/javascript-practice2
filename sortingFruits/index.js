d98e1e8f12270e84ad40a06e856cfd8f8189fbcd
abdu <abdualkerim01@gmail.com>
2026-02-21 23:04:47 +0300

    from else if to else

--- sortingFruits/index.js
+++ sortingFruits/index.js
@@ -4,4 +4,16 @@ let orangeShelf = document.getElementById("orange-shelf");
 
 // Create a function that puts the apples onto the appleShelf
 // and the oranges onto the orangeShelf. Use a for loop,
-// a conditional statement, and the textContent property.
+// a conditional statement, and the textContent propert
+
+function sortFruit(fruit) {
+    for (let i = 0; i < fruit.length; i++) {
+        if (fruit[i] === "🍎") {
+            appleShelf.textContent += fruit[i];
+        } else {
+            orangeShelf.textContent += fruit[i];
+        }
+    }
+}
+
+sortFruit(fruit);
